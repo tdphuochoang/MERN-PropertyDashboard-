e61ed4382e1411eae6a9e2fcef89e6aeda083e92
Hoang Tran <tdll.trdphoang@gmail.com>
2023-04-17 21:40:50 -0700

    MongoDB setup and connected

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
+
+dotenv.config();
+
+const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", (req, res) => {
+	res.send({ message: "Hello World" });
+});
+
+const startServer = async () => {
+	try {
+		//Connect to the database
+		connectDB(process.env.MONGODB_URL);
+
+		app.listen(8080, () => {
+			console.log("Server started on port http://localhost:8080");
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+startServer();

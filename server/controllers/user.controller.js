369eaf3afb7521234cc5215f64cd0062f3e02ad5
Hoang Tran <tdll.trdphoang@gmail.com>
2023-04-17 22:06:03 -0700

    Created user and property routes

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import User from "../mongodb/models/user.js";
+
+//GET ALL USERS
+const getAllUsers = async (req, res) => {};
+
+//CREATE USER
+const createUser = async (req, res) => {};
+
+//GET USER'S INFO BY ID
+const getUserInfoByID = async (req, res) => {};
+
+export { getAllUsers, createUser, getUserInfoByID };
